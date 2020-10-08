dfeddaab83d79d66b3f0c2b355a5eeca5793206f
nitin1411 <61113244+nitin1411@users.noreply.github.com>
2020-10-08 14:17:11 +0530

    Add files via upload

---
+++ SVM_Assignment.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 16:06:31 2020
+
+@author: nitin
+"""
+
+# SVM Classification
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+
+#classification of forest fires using SVM
+data = read_csv("C:\\Users\\nitin\\Desktop\\Assignments\\SVM\\forestfires.csv")
+arr = data.values
+x = arr[:,2:30]
+y = arr[:,30]
+kfold = KFold(n_splits = 10, random_state = 7 )
+mod = SVC()
+result = cross_val_score(mod, x,y, cv = kfold)
+print(result)
+
+
+#classification of salary data using SVM
+from pandas import get_dummies
+sal_data = read_csv("C:\\Users\\nitin\\Desktop\\Assignments\\SVM\\SalaryData_Train(1).csv")
+train_sd = get_dummies(sal_data) #creating dummy variables on train data
+train_sd.head()
+
+test_sal_data = read_csv("C:\\Users\\nitin\Desktop\\Assignments\\SVM\\SalaryData_Test(1).csv")
+test_sd = get_dummies(test_sal_data)
+arr_train = train_sd.values
+arr_test = test_sd.values
+x_train = arr_train[:,0:103]
+y_train = arr_train[:,103:105]
+kf = KFold(n_splits = 10, random_state = 7)
+mod1 = SVC()
+result1 = cross_val_score(mod1, x_train, y_train, cv = kf)
+print(result1)
+
+#model validation on test data
+x_test = arr_test[:,0:103]
+y_test = arr_test[:,103:105]
+result2 = cross_val_score(mod1, x_test, y_test, cv = kf)
+print(result2)
