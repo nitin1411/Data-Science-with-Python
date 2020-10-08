38ad68c4248c0c07a5bee77d11dd1bbdf5e1bd7a
nitin1411 <61113244+nitin1411@users.noreply.github.com>
2020-10-08 14:27:39 +0530

    Add files via upload

---
+++ Logistic_Regression.py
@@ -0,0 +1,16 @@
+# Logistic Regression Classification
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+dataframe = read_csv(filename, names=name)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+num_folds = 10
+kfold = KFold(n_splits=10, random_state=7)
+model = LogisticRegression()
+mod=model.fit(X,Y)
+prediction=mod.predict(X)
+results = cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
